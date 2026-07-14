2ac7100530d30d433f07e2d7dc74ad91891e5705
arez992 <arez.abubakr92@gmail.com>
2026-07-14 16:30:14 +0300

    feat: add ZAOS public API

---
+++ lib/zaos/index.ts
@@ -0,0 +1,17 @@
+// ZAOS Core
+export * from "./core/AIRole";
+export * from "./core/DecisionLifecycle";
+export * from "./core/DecisionState";
+export * from "./core/ExecutiveContext";
+
+// ZAOS Policy
+export * from "./policy/PolicyTypes";
+
+// ZAOS Tasks
+export * from "./tasks/TaskTypes";
+
+// ZAOS Memory
+export * from "./memory/MemoryTypes";
+
+// ZAOS Reports
+export * from "./reports/ReportTypes";
